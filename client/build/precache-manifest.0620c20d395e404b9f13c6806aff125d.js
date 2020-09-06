b850e86f9ff5091fdf929cb93f840dc69f5e7f06
mebelh <memetbelualov@bk.ru>
2020-09-06 21:47:07 +0300

    build

---
+++ client/build/precache-manifest.0620c20d395e404b9f13c6806aff125d.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "15c57591a5c823326c6746baf19feed0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b03dc2405ddf914fdfdd",
+    "url": "/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "b3be11d211acfb0393b0",
+    "url": "/static/css/main.52a6e86c.chunk.css"
+  },
+  {
+    "revision": "b03dc2405ddf914fdfdd",
+    "url": "/static/js/2.0abb7be3.chunk.js"
+  },
+  {
+    "revision": "ed6d44160be3da7ea632241f05f7ba30",
+    "url": "/static/js/2.0abb7be3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b3be11d211acfb0393b0",
+    "url": "/static/js/main.2f15bf72.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  },
+  {
+    "revision": "3001cff3181eefd8db327cd724309a09",
+    "url": "/static/media/Caveat-Bold.3001cff3.ttf"
+  },
+  {
+    "revision": "e3291f33a1ec2425a17ac70be005d67b",
+    "url": "/static/media/HarryPotter.e3291f33.ttf"
+  },
+  {
+    "revision": "e2e4e9b17c5691531668d61466cc4fa5",
+    "url": "/static/media/zashita.e2e4e9b1.png"
+  }
+]);
